ce52b31574e92364992571de18e0be82c89ad657
Lavish Sharma <lavish13sharma@gmail.com>
2022-05-16 11:20:14 +0530

    16 may

--- script.js
+++ script.js
@@ -59,3 +59,14 @@ document.addEventListener("scroll", () => {
 });
 
 backToTopButton.addEventListener("click", goToTop);
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby7DQxZv09f2h2185BHGZKGGESuuRokczJzWI-OLx2V7HGbj55fL8p5nSRuDfRpGBAc/exec'
+const form = document.forms['google-sheet']
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+.then(response => $("#form_alerts").html("<div class='alert alert-success'>Contact message sent successfully.</div>"))
+.catch(error => $("#form_alerts").html("<div class='alert alert-danger'>Contact message not sent.</div>"))
+})
